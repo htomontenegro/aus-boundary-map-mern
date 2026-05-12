c3c01435f428f146b9e5d765a2ecb96838120d8c
root <root@srv1660587.hstgr.cloud>
2026-05-12 10:55:04 +0000

    fixes for mongodb in prd

---
+++ server/ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps: [{
+    name: 'aus-boundary-api',
+    script: 'start.sh',
+    interpreter: 'bash',
+    cwd: '/var/www/australian-boundaries.builtbyhumberto.tech/server'
+  }]
+}
